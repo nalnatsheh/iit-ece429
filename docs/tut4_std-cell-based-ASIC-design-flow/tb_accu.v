e0004b9361c1837e927f16a0a6af8f1ce1d2ffe8
Nader Alnatsheh <97265693+nalnatsheh@users.noreply.github.com>
2024-10-15 14:00:06 -0500

    Update tb_accu.v

--- docs/tut4_std-cell-based-ASIC-design-flow/tb_accu.v
+++ docs/tut4_std-cell-based-ASIC-design-flow/tb_accu.v
@@ -1,1 +1,39 @@
+module tb_accu;
 
+reg     [7:0]   in;
+reg             clk;
+reg             rst;
+wire    [7:0]   accu;
+
+accu dut(.in(in),
+         .accu(accu),
+         .clk(clk),
+         .rst(rst)
+);
+
+initial
+ begin
+        $shm_open("shm.db", 1);
+        $shm_probe("AS");
+        #50 $finish;
+        #100 $shm_close();
+ end
+
+initial
+ begin
+  clk = 1'b0;
+  forever
+   begin
+   #5 clk = ~clk;
+   $display("At time: %d The Accumulator Output=%d",$time, accu);
+   end
+ end
+
+initial
+ begin
+        #0 rst <= 1;
+           in  <= 1;
+        #5 rst <= 0;
+ end
+
+endmodule
